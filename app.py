afd6ff4ce17caf2ddac7ff6c31125f6556878550
pomynchuky-commits <pomynchuky@gmail.com>
2026-03-12 22:32:33 +0200

    Add function to sum two numbers with user input

---
+++ app.py
@@ -0,0 +1,8 @@
+def add(a, b):
+    return a + b
+
+
+if __name__ == "__main__":
+    a = float(input("Введіть перше число: "))
+    b = float(input("Введіть друге число: "))
+    print("Сума:", add(a, b))
